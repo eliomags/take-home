968a7e8cd824751cfc138c2384fb3cc46a7da66b
Elshan Magsudov <elshanmagsudov@Elshans-MacBook-Pro.local>
2023-06-10 02:03:50 -0600

    playing with java test

--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -1,6 +1,20 @@
 package fun;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+import org.junit.jupiter.api.Test;
+
 public class GameTests {
 
-
+    @Test
+    public void testGetGame() {
+        given()
+            .param("name", "John")
+        .when()
+            .get("http://localhost:8080/game")
+        .then()
+            .statusCode(200)
+            .body(matchesJsonSchemaInClasspath("game-schema.json"));
+    }
 }
